a75e72b1cc0ee6b18c442bfb168df830a172b81f
as7736 <134200399+as7736@users.noreply.github.com>
2025-04-25 14:06:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
-pip install huggingface_hub[hf_xet]
-pip install torch --upgrade
-pip uninstall -y numpy spacy thinc
-pip install numpy==1.26.4
-pip install spacy
-pip uninstall -y transformers
-pip install --force-reinstall transformers
-python -c "import transformers; print(dir(transformers))"
